895e0f2fe22d374f2de5ea3ea9eb0f77c48445a2
Marcos Barreto <msb5@cin.ufpe.br>
2017-10-21 19:12:16 -0300

    classificador naive bayes para uma categoria

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,14 +1,38 @@
 import script
 from script import text_processing
 
-def naive_bayes():
+categories = ["acq","corn","crude","earn","grain","interest","money-fx","trade","veg-oil","wheat"]
+
+def count_words():
 	bow = text_processing()
-
-	for b in bow:
-		result = [(x,b.treinamento.count(x)) for x in b.treinamento]
-		print(result)
 	
+	result = []	
+	for b in bow:
+		d = {}
+		for x in b["treinamento"]:
+			if x in d:
+				d[x] += 1
+			else:
+				d[x] = 1
 
+		result.append({"categoria": b["categoria"], "words": d, "qtde_tokens": len(d), "qtde": len(b["treinamento"])})
+	
+	return result
+
+def naive_bayes(text):
+	count = count_words()
+	
+	result = []
+	for c in count:
+		prob_c = 1
+		for word in text:
+			if word in c["words"]:
+				prob_c *= (c["words"][word] + 1) / (c["qtde"] + c["qtde_tokens"])
+			else:
+				prob_c *= 1 / (c["qtde"] + c["qtde_tokens"])
+		
+		result.append({"categoria": c["categoria"], "probabilidade": prob_c})
+
+	print(result)
+	
 naive_bayes()
-
-
--- script.py
+++ script.py
@@ -7,7 +7,8 @@ nltk.download('punkt')
 
 def text_processing():
     porter = nltk.PorterStemmer()
-    categories = ["acq","corn","crude","earn","grain","interest","money-fx","trade","veg-oil","wheat"]
+    # categories = ["acq","corn","crude","earn","grain","interest","money-fx","trade","veg-oil","wheat"]
+    categories = ["corn"]
     bow = []
     # Documents in a category
     for c in categories:
