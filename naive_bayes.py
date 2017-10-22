9f7fa802f211c4731326cf43920cc17981ca0f5e
Marcos Barreto <msb5@cin.ufpe.br>
2017-10-21 21:13:12 -0300

    atualizacao do classificador

--- naive_bayes.py
+++ naive_bayes.py
@@ -3,8 +3,9 @@ from nltk import word_tokenize
 from nltk.corpus import reuters
 import script
 from script import text_processing
+from decimal import *
 
-categories = ["acq","corn","crude","earn","grain","interest","money-fx","trade","veg-oil","wheat"]
+# categories = ["acq","corn","crude","earn","grain","interest","money-fx","trade","veg-oil","wheat"]
 
 def count_words():
 	bow = text_processing()
@@ -23,22 +24,28 @@ def count_words():
 	return result
 
 def naive_bayes(text,count):
-	#print(text)
-	
 	result = []
 	for c in count:
 		prob_c = 1
 		for word in text:
 			if word in c["words"]:
-				prob_c *= (c["words"][word] + 1) / (c["qtde"] + c["qtde_tokens"])
+				prob_c *= Decimal(c["words"][word] + 1) / Decimal(c["qtde"] + c["qtde_tokens"])
 			else:
-				prob_c *= 1 / (c["qtde"] + c["qtde_tokens"])
-				
+				prob_c *= Decimal(1) / Decimal(c["qtde"] + c["qtde_tokens"])				
 		
 		result.append({"categoria": c["categoria"], "probabilidade": prob_c})
 
-	print(result)
+	return result
 
+def high_class(categories_prob):
+	category = ""
+	maximum = Decimal(0)
+	for c in categories_prob:
+		if c["probabilidade"] > maximum:
+			category = c["categoria"]
+			maximum = c["probabilidade"]
+
+	return category
 
 def main():
 	count = count_words()
@@ -51,14 +58,23 @@ def main():
 		
 		stemmer = []
 		main_test = []
-		
-		raw = reuters.raw(test_docs[0])
-		porter = nltk.PorterStemmer()
-		tokens = word_tokenize(raw)
-		stemmer = [porter.stem(t)for t in tokens]
-		naive_bayes(stemmer,count)
+		acc = 0
+		err = 0
+		for t in test_docs:
+			raw = reuters.raw(t)
+			porter = nltk.PorterStemmer()
+			tokens = word_tokenize(raw)
+			stemmer = [porter.stem(t)for t in tokens]
+			result = naive_bayes(stemmer,count)
 
-	
-
+			if c == high_class(result):
+				acc += 1
+				print("ACERTOU")
+			else:
+				err += 1
+				print("ERRRRROU")
+			
+		print(acc)
+		print(err)
 
 main()
